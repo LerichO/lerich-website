b0db6db9dbb0a107379167ab642347a5485597a8
Lerich Osay <lerichyaso@gmail.com>
2024-06-25 09:34:23 -0500

    scroll to sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css'
 
 import { TypeAnimation } from 'react-type-animation'
 
 function App() {
+
+  const home = useRef(null)
+  const about = useRef(null)
+  const projects = useRef(null)
+  const stats = useRef(null)
+
+  const scrollTo = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
-        <TypeAnimation
-          sequence={[
-            'Lerich Osay',
-            5000,
-            '',
-            100
-          ]}
-          wrapper="span"
-          speed={50}
-          style={{ fontSize: '2em', display: 'inline-block' }}
-          repeat={Infinity}
-        />
-      </h1>
-
+      <div>
+        <ul className="flex flex-row w-screen gap-10 p-10">
+          <li onClick={() => scrollTo(home)}>Home</li>
+          <li onClick={() => scrollTo(about)}>About Me</li>
+          <li onClick={() => scrollTo(projects)}>Projects</li>
+          <li onClick={() => scrollTo(stats)}>Cool Stats</li>
+        </ul>
+      </div>
+      <div ref={home}>
+        <h1>
+          <TypeAnimation
+            sequence={[
+              'Lerich Osay',
+              5000,
+              '',
+              100
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: '2em', display: 'inline-block' }}
+            repeat={Infinity}
+          />
+        </h1>
+      </div>
       <div className="card flex flex-col gap-4">
         <div className='flex flex-row gap-4 justify-center'>
           <button onClick={() => window.open('https://www.linkedin.com/in/lerichrosay', '_blank')}>
@@ -36,6 +59,20 @@ function App() {
           🛠 Work in progress 🛠
         </p>
       </div>
+
+      <div className='p-10'>
+        <div ref={about} className="h-screen">
+          About
+        </div>
+        <div ref={projects} className="h-screen">
+          Projects
+        </div>
+        <div ref={stats} className="h-screen">
+          Some Fun Stats
+        </div>
+
+      </div>
+
     </>
   )
 }
